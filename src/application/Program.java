f365ea14045c71b0b0d771a2495a85b0733def17
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-03 00:04:00 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMath;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		ChessMath chessMath = new ChessMath();
+		UI.printBoard(chessMath.getPieces());
 		
 	}
 
