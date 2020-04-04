5842970444e5b4396d3bb771f86fcec57c1eef50
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-03 21:41:37 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMath chessMath = new ChessMath();
-		UI.printBoard(chessMath.getPieces());
-		
-	}
 
+		while (true) {
+			UI.printBoard(chessMath.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMath.performChessMove(source, target);  
+		}
+	}
 }
